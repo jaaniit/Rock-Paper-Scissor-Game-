563f040403d417f9521c5ddfdb2b26e6f7e036d9
Janith Widanegamage <120152620+jaaniit@users.noreply.github.com>
2024-01-08 07:26:39 +0530

    Add files via upload

---
+++ RockPaper Scissors/Readme.txt
@@ -0,0 +1,3 @@
+This is the rock paper scissor game.it can be play one person.
+first we run the program.next we can type a rock or paper ot scissor.Then will be give output you wi or loss!
+E n j o y  m y  g a m e !
---
+++ RockPaper Scissors/rockpaperscissor.py
@@ -0,0 +1,36 @@
+import random
+def get_choices():
+    player_choice=("Enter a choice (rock,paper,scissor:")
+    options = ["rock","paper","scissor"]
+    computer_choice=random.choice(options)
+    choices = {"player":player_choice, "computer":computer_choice}
+    return choices
+
+def check_win(player, computer):
+    print(f"You choose {player}, computer choose {computer}")
+    if player == computer:
+        return "It's a tie!"
+    elif player == "rock":
+        if computer == "scissor":
+            return "Rock smashed scissor! You win!"
+        else:
+            return "Paper covers rock! You lose."
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissor cuts paper! You lose."
+    elif player == "scissor":
+        if computer == "paper":
+            return "Scissor cuts paper! You win!"
+        else:
+            return "Rock smashed scissor! You lose."
+
+choices = get_choices()
+result = check_win(choices["player"],choices["computer"])
+print(result)
+        
+            
+        
+            
+
